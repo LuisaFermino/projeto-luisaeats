ef7fde3c991c2b6b6482a2c63fdcec21eb34b039
LuisaFermino <souzafermino49@gmail.com>
2023-09-28 11:17:52 -0300

    Mensagem pronta com configuraçao finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -129,13 +129,13 @@ function confirmarPedido() {
     const enderecoCliente = prompt("Informe seu Endereço: ");
 
     const mensagemZap = encodeURI(`Olá, gostaria de fazer o pedido:\n
-    - Prato: ${nomePrato}        R$ ${valorPatualizado}
-    - Bebida: ${nomeBebida}      R$ ${valorBatualizado}
-    - Sobremesa: ${nomeSobremesa}          R$ ${valorSatualizado}
-    \nTotal: R$ ${valorAtualizado}\n
+  - Prato: ${nomePrato}        R$ ${valorPatualizado}
+  - Bebida: ${nomeBebida}      R$ ${valorBatualizado}
+  - Sobremesa: ${nomeSobremesa}          R$ ${valorSatualizado}
+    \n*Total: R$ ${valorAtualizado}*\n
     Nome: ${nomeCliente}
     Endereço: ${enderecoCliente}
-    \n**Seu pedido chegará em 30min**`);
+    \n*Seu pedido chegará em 30min*`);
 
     window.location.href = `https://wa.me/5514998812492?text=${mensagemZap}`;
   }
